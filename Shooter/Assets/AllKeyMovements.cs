d8de1343ba57656ad4898a6b6d1127945680df38
DropBear25 <60543506+DropBear25@users.noreply.github.com>
2020-03-09 21:00:10 +0000

    next triggers

---
+++ Shooter/Assets/AllKeyMovements.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+    public float mSpeed;
+
+    Vector3 mvelocity;
+
+    void Start()
+    {
+
+    }
+    private void Update()
+    {
+        mvelocity = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            mvelocity.z = 1.0f;
+           
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            mvelocity.z = -1.0f;
+          
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            mvelocity.x = -1.0f;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            mvelocity.x = 1.0f;
+        }
+        transform.Translate(mvelocity.normalized * Time.deltaTime * mSpeed);
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0.0f, 40 * Time.deltaTime, 0.0f);
+
+        }else if(Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0.0f, -40.0f * Time.deltaTime, 0.0f);
+        }
+    }
+}
+    
---
+++ Shooter/Assets/PlayerRed.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRed : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    public float mSpeed;
+
+    Vector3 mvelocity;
+
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+        mvelocity = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            mvelocity.z = 1.0f;
+
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            mvelocity.z = -1.0f;
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            mvelocity.x = -1.0f;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            mvelocity.x = 1.0f;
+        }
+        transform.Translate(mvelocity.normalized * Time.deltaTime * mSpeed);
+
+
+
+
+
+
+
+
+    }
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
